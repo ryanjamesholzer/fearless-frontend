a7f455387aa117e87ad2543a46181f62f469812b
Ryan Holzer <ryanjames.holzer@gmail.com>
2022-11-28 14:30:07 -0700

    stretch goal check status

---
+++ ghi/js/check-status.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', async () => {
+
+
+    const payloadCookie =  await cookieStore.get('jwt_access_payload');
+    //console.log(payloadCookie);
+    //console.log(typeof(payloadCookie));
+
+    if (payloadCookie) {
+        //const encodedPayload = JSON.parse(payloadCookie.value);
+
+        const decodedPayload = atob(payloadCookie.value);
+
+        const payload = JSON.parse(decodedPayload);
+
+        console.log(payload);
+
+        if (payload.user.perms.includes("events.add_conference") && payload.user.perms.includes("events.add_location")) {
+            const locationLinkTag = document.getElementById('hidden-location');
+            locationLinkTag.classList.remove("d-none");
+            const conferenceLinkTag = document.getElementById('hidden-conference');
+            conferenceLinkTag.classList.remove('d-none');
+        }
+
+
+    }
+})
