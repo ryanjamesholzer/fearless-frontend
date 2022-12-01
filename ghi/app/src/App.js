3f3ff2f70a247fc7b6169cc5efc7726e7d572189
Ryan Holzer <ryanjames.holzer@gmail.com>
2022-11-30 19:40:00 -0700

    worked on attendee sign up

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -3,13 +3,15 @@ import Nav from './Nav';
 import AttendeesList from './AttendeesList';
 import LocationForm from './LocationForm';
 import ConferenceForm from './ConferenceForm';
+import AttendeeSignUpForm from './AttendeeSignUpForm';
 
 function App(props) {
   return (
     <React.Fragment>
       <Nav />
       <div className="container">
-        <ConferenceForm/>
+        <AttendeeSignUpForm/>
+        {/*<ConferenceForm/> */}
         {/* < LocationForm /> */}
         {/*<AttendeesList attendees={props.attendees}/>*/}
       </div>
